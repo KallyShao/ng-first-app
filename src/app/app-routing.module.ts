e8037cc288442beee1e01ac99b554cc0feb26570
Kally Shao <188963758@qq.com>
2019-03-05 17:52:31 +0800

    m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,11 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path:'home',
     component: HomeComponent
   },
